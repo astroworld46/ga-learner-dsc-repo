83a29607c187bfb425bf8cfa278cfd4c60e6a7bb
Nakul Shivakumar <nakulshivakumar@outlook.com>
2021-02-21 16:15:46 +0530

    Code file created.

---
+++ Loan-Approval-Analysis/code.py
@@ -0,0 +1,68 @@
+# --------------
+# Importing header files
+import numpy as np
+import pandas as pd
+from scipy.stats import mode 
+import warnings
+warnings.filterwarnings('ignore')
+
+
+#Reading file
+bank_data = pd.read_csv(path)
+
+
+#Code starts here
+bank=pd.read_csv(path)
+categorical_var=bank.select_dtypes(include='object')
+print("Categorical variables:",categorical_var)
+
+#Code for numerical values
+numerical_var=bank.select_dtypes(include='number')
+print("Numerical Variables:",numerical_var)
+
+#load the dataset and drop the Loan ID
+banks=bank.drop(columns='Loan_ID')
+
+#check all the missing values fillled.
+print(banks.isnull().sum())
+
+#apply mode
+bank_mode=banks.mode().iloc[0]
+
+#Fill the missing values with
+banks.fillna(bank_mode,inplace=True)
+
+#check the avg_loan_amount
+avg_loan_amount=banks.pivot_table(values=["LoanAmount"],index=["Gender","Married","Self_Employed"],aggfunc=np.mean)
+
+#code for loan approved for self employed
+loan_approved_se=banks.loc[(banks["Self_Employed"]=="Yes")&(banks["Loan_Status"]=="Y"),["Loan_Status"]].count()
+
+#code for loan approved for non self employed
+loan_approved_nse=banks.loc[(banks["Self_Employed"]=="No")&(banks["Loan_Status"]=="Y"),["Loan_Status"]].count()
+
+#percentage of loan approved for self employed
+percentage_se=(loan_approved_se*100/614)
+percentage_se=percentage_se[0]
+print(percentage_se)
+
+#percentage of loan for non self employed
+percentage_nse=(loan_approved_nse*100/614)
+percentage_nse=percentage_nse[0]
+print(percentage_nse)
+
+#loan amount term
+loan_term=banks['Loan_Amount_Term'].apply(lambda x: int(x)/12)
+big_loan_term=len(loan_term[loan_term>=25])
+print(big_loan_term)
+
+columns_to_show=['ApplicantIncome','Credit_History']
+
+loan_groupby=banks.groupby(['Loan_Status'])
+loan_groupby=loan_groupby[columns_to_show]
+
+#check the mean value
+mean_values=loan_groupby.agg([np.mean])
+print(mean_values)
+
+
